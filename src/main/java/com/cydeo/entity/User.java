70ab8332312a4999045bab393073c2a95b3e49f3
selimdemirer <selimmdemirerr@gmail.com>
2023-09-29 14:14:14 +0300

    I put the dependencies: Spring Data JPA and Postgres Driver
    I connected my app to the DB through application.properties
    
    I started to create entity classes

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,13 +1,20 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.Gender;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User extends BaseEntity {
 
     private String firstName;
@@ -16,19 +23,9 @@ public class User extends BaseEntity {
     private String passWord;
     private boolean enabled;
     private String phone;
-    private Role role;
-    private Gender gender;
 
-    public User(Long id, LocalDateTime insertDateTime, Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String userName, String passWord, boolean enabled, String phone, Role role, Gender gender) {
-        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.passWord = passWord;
-        this.enabled = enabled;
-        this.phone = phone;
-        this.role = role;
-        this.gender = gender;
-    }
+    private Role role;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 
 }

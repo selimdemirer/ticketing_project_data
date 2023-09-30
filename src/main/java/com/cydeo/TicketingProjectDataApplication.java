2cb0d197576b0fe2c7fe3f53801ff0b12aa6cd4f
selimdemirer <selimmdemirerr@gmail.com>
2023-09-30 15:32:24 +0300

    //I am trying to add bean in the container through @Bean annotation
        //Create a class annotated with @Configuration
    
        //Write a method which return the object that you trying to add in the container
    
        //Annotate this method with @Bean

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -10,4 +10,13 @@ public class  TicketingProjectDataApplication {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
 
+    //I am trying to add bean in the container through @Bean annotation
+
+
+    //Create a class annotated with @Configuration
+
+    //Write a method which return the object that you trying to add in the container
+
+    //Annotate this method with @Bean
+
 }

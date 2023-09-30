4baf5dd9d3adb5caaa4d017e6e764d9a64711fa3
selimdemirer <selimmdemirerr@gmail.com>
2023-09-30 15:10:54 +0300

    but our method is looking for RoleDTO

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,13 @@ import java.util.List;
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+
     //controller called me and requesting all RoleDTOs so it can show in the drop-down in the UI
     //I need to make a call to DB and get all the roles from table
     //go to repository and find a service(method) which gives me the roles from DB
@@ -16,7 +25,9 @@ public class RoleServiceImpl implements RoleService {
     @Override
     public List<RoleDTO> listAllRoles() {
 
-        return null;
+        List<Role> roleList = roleRepository.findAll(); // derived query is coming from Jpa
+
+        return roleList; // but our method is looking for RoleDTO
     }
 
     @Override

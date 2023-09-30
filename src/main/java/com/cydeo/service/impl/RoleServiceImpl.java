4e2332436dc37e8c86c2e1a17a803cd5ba500b07
selimdemirer <selimmdemirerr@gmail.com>
2023-09-30 15:04:30 +0300

    controller called me and requesting all RoleDTOs so it can show in the drop-down in the UI
    I need to make a call to DB and get all the roles from table
    
    go to repository and find a service which gives me the roles from DB

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -8,8 +8,14 @@ import java.util.List;
 
 @Service
 public class RoleServiceImpl implements RoleService {
+
+    //controller called me and requesting all RoleDTOs so it can show in the drop-down in the UI
+    //I need to make a call to DB and get all the roles from table
+    //go to repository and find a service(method) which gives me the roles from DB
+    //how will I call any service here?
     @Override
     public List<RoleDTO> listAllRoles() {
+
         return null;
     }
 

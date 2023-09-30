67e793c6a5e12b5a701616d81717db6e18a83f11
selimdemirer <selimmdemirerr@gmail.com>
2023-09-30 15:44:44 +0300

    in accordance with solid principles we created a separate package and class for mappers

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,22 +1,27 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication  // this covers @Configuration (also it covers @SpringBootConfiguration, @EnableAutoConfiguration and @ComponentScan)
 public class  TicketingProjectDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
 
-    //I am trying to add bean in the container through @Bean annotation
-
-
-    //Create a class annotated with @Configuration
-
-    //Write a method which return the object that you trying to add in the container
-
+    //Write a method which return the object that you are trying to add in the container
     //Annotate this method with @Bean
+
+    @Bean
+    public ModelMapper mapper(){
+
+        return new ModelMapper();
+
+    }
+
+
 
 }
---
+++ src/main/java/com/cydeo/mapper/RoleMapper.java
@@ -0,0 +1,7 @@
+package com.cydeo.mapper;
+
+public class RoleMapper {
+
+
+
+}

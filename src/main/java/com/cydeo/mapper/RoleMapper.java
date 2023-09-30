875faa5dccec134b018501bce4bc74ee064e4de9
selimdemirer <selimmdemirerr@gmail.com>
2023-09-30 16:27:20 +0300

    PICTURE 7 STRUCTURE is completed

--- src/main/java/com/cydeo/mapper/RoleMapper.java
+++ src/main/java/com/cydeo/mapper/RoleMapper.java
@@ -1,7 +1,30 @@
 package com.cydeo.mapper;
 
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RoleMapper {
 
+    private ModelMapper modelMapper;
 
+    public RoleMapper(ModelMapper modalMapper) {
+        this.modelMapper = modalMapper;
+    }
+
+    public Role convertToEntity(RoleDTO dto){
+
+        return modelMapper.map(dto, Role.class);
+
+    }
+
+    public RoleDTO convertToDTO(Role entity){
+
+        return modelMapper.map(entity,RoleDTO.class);
+
+    }
 
 }
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -2,21 +2,24 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
 import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
         this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
     }
-
 
     //controller called me and requesting all RoleDTOs so it can show in the drop-down in the UI
     //I need to make a call to DB and get all the roles from table
@@ -27,7 +30,13 @@ public class RoleServiceImpl implements RoleService {
 
         List<Role> roleList = roleRepository.findAll(); // derived query is coming from Jpa
 
-        return roleList; // but our method is looking for RoleDTO
+        //I have Role entities from DB
+        //I need to convert those Role entities to DTOs
+        //I need to use ModelMapper
+        //I already created a class called RoleMapper and there are methods for me that will make this conversion
+        //All I need to do is DI and implement the methods!
+
+        return roleList.stream().map(roleMapper::convertToDTO).collect(Collectors.toList()); //because method converts only one object, but we created a for loop thru stream!;
     }
 
     @Override

0028cd539e0cd4dc5289ca822b5fcf66a6b29f7b
selimdemirer <selimmdemirerr@gmail.com>
2023-09-30 12:07:31 +0300

    Revert "I deleted all classes and interfaces under service and impl package since I will re-design them"
    This reverts commit 88f0be4594a68693cbded987d77b0dad92259a25.

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String passWord;
+    private boolean enabled;
+    private String phone;
+    @ManyToOne // Because one role has many users. Many side is always the owner!
+    private Role role;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+}

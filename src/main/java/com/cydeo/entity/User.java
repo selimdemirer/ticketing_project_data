cdabfe72c5b68448cbd0afd4a57e89631585f240
selimdemirer <selimmdemirerr@gmail.com>
2023-10-02 06:04:20 +0300

    findAll is coming from JpaRepository. I need to create my own query. And also, I need to add conditions to other methods or create my owns. But this is so much work. Spring Boot says, you can use @Where annotation top of the class. So I put it top of the User (Entity) class

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -2,6 +2,7 @@ package com.cydeo.entity;
 
 import com.cydeo.enums.Gender;
 import lombok.*;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
+@Where(clause = "is_deleted=false")
 public class User extends BaseEntity {
 
     private String firstName;

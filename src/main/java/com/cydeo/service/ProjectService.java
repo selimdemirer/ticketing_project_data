09369c3a00e89a142b2f22e4256466dc1e820ce4
selimdemirer <selimmdemirerr@gmail.com>
2023-10-02 06:51:34 +0300

    Project Page is started

---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,15 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+
+import java.util.List;
+
+public interface ProjectService {
+
+    ProjectDTO getByProjectCode(String code);
+    List<ProjectDTO> listAllProjects();
+    void save(ProjectDTO projectDTO);
+    void update(ProjectDTO projectDTO);
+    void delete(String code);
+
+}

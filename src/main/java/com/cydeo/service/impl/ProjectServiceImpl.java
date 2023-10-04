9be1009532c4e1b3fcd602f9ed8fec3fe349c786
selimdemirer <selimmdemirerr@gmail.com>
2023-10-04 15:03:03 +0300

    Project Status is done with one error that I haven't solved yet

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,11 +1,16 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.Project;
+import com.cydeo.entity.User;
 import com.cydeo.enums.Status;
 import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.ProjectRepository;
 import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,10 +21,16 @@ public class ProjectServiceImpl implements ProjectService {
 
     private final ProjectRepository projectRepository;
     private final ProjectMapper projectMapper;
+    private final UserService userService;
+    private final UserMapper userMapper;
+    private final TaskService taskService;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper, UserService userService, UserMapper userMapper, TaskService taskService) {
         this.projectRepository = projectRepository;
         this.projectMapper = projectMapper;
+        this.userService = userService;
+        this.userMapper = userMapper;
+        this.taskService = taskService;
     }
 
     @Override
@@ -52,7 +63,7 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     public void update(ProjectDTO projectDTO) {
 
-        Project project = projectRepository.findByProjectCode(projectDTO.getProjectCode()); //We are going to DB and bring this project because this project has Id and we nee d taht Id
+        Project project = projectRepository.findByProjectCode(projectDTO.getProjectCode()); //We are going to DB and bring this project because this project has Id and we need that Id
 
         Project convertedProject = projectMapper.convertToEntity(projectDTO);
 
@@ -81,4 +92,30 @@ public class ProjectServiceImpl implements ProjectService {
         projectRepository.save(project);
 
     }
+
+    @Override
+    public List<ProjectDTO> listAllProjectDetails() {
+
+        //Final Goal: hey DB, give me all projects assigned to manager in the system
+                                //this part will come security in the future
+        UserDTO currentUserDTO = userService.findByUserName("dasd");
+        User user = userMapper.convertToEntity(currentUserDTO);
+
+        List<Project> list = projectRepository.findAllByAssignedManager(user);
+
+
+
+        return list.stream().map(project -> {
+
+            ProjectDTO obj = projectMapper.convertToDTO(project);
+                    obj.setUnfinishedTaskCounts(taskService.totalNonCompletedTask(project.getProjectCode()));
+                    obj.setCompleteTaskCounts(taskService.totalCompletedTask(project.getProjectCode()));
+
+            return obj;
+
+        }
+        ).collect(Collectors.toList());
+    }
+
+
 }
